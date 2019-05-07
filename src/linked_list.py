6a4a71f39ddbdd18bf90441bf4b84313ac7f7c5a
fangc <saij84@gmail.com>
2019-05-07 22:00:34 +1000

    update day 15

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,21 +1,33 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
+
 class Solution:
-    def display(self,head):
+    def display(self, head):
         current = head
         while current:
-            print(current.data,end=' ')
+            print(current.data, end=' ')
             current = current.next
 
-    def insert(self,head,data):
-    #Complete this method
+    def insert(self, head, data):
+        # Complete this method
+        new_node = Node(data)
+        if head == None:
+            self.head = new_node
+            return self.head
+        n = self.head
+        while n.next:
+            n = n.next
+        n.next = new_node
+        return self.head
 
 mylist= Solution()
-T=int(input())
-head=None
+T = 4
+
+head = None
+
 for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)
-mylist.display(head);
+    data = [3, 2, 1, 0]
+    head = mylist.insert(head, data[i])
+mylist.display(head)
